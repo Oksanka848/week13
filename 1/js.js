9c65129567d4ed39b2363e0a930c5e0bd4bb540c
Oksanaka848 <okmitka@mail.ru>
2022-10-15 17:24:40 +0300

    ch

--- 1/js.js
+++ 1/js.js
@@ -1,10 +1,12 @@
-window.onload = init;
+window.onload = () => {
+    init();
+  };
 
-function init(){
+init = () => {
     let button = document.getElementById("addButton")
     button.onclick = handleButtonClick;
 }
 
-function handleButtonClick() {
+    handleButtonClick = () => {
     let name = prompt( "Ваше имя?" );
     alert(`Привет, ${name} !`);}
